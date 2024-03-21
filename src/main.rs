bf6566227875e6d51ec5dc826e6b77ecb9d257b4
Kaiskii <24761071+Kaiskii@users.noreply.github.com>
2024-03-21 15:37:14 +0800

    feat: initial game loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,100 @@
-use rand::Rng;
+use std::env;
+use std::io;
+use std::sync::Arc;
+use reqwest::Client;
+use reqwest::Result;
+use dotenvy::dotenv;
+use serde::{Deserialize};
 
-fn main() {
-    println!("Hello, {}!", rand::thread_rng().gen_range(1..101));
+#[derive(Deserialize)]
+struct WordAPI {
+    word: String,
+}
+
+async fn get_random_word() -> Result<String> {
+    let request_url = env::var("WORD_API").expect("WORD_API not found") + "/word";
+    let response = Client::new()
+        .get(request_url)
+        .send()
+        .await?
+        .json::<WordAPI>()
+        .await?;
+
+    Ok(response.word)
+}
+
+fn game_loop(word_ref: &str) {
+    let mut buffer = String::from("");
+    let mut correct = String::from("");
+    let mut history = String::from("");
+    let mut blank = String::from("");
+
+    loop {
+        blank = String::from("");
+        println!("{}", word_ref);
+
+        // Logic to process words
+        for c in word_ref.chars() {
+            if buffer.len() > 0 {
+                let answer = buffer.chars().next().unwrap();
+                if c == answer
+                {
+                    blank += &answer.to_string();
+                    if !correct.contains(&answer.to_string()) {
+                        correct += &answer.to_string();
+                    }
+                } else if correct.contains(c) {
+                    blank += &c.to_string();
+                } else {
+                    blank += "|";
+                }
+            } else {
+                blank += "|";
+            }
+
+            // Win Condition
+            if blank == word_ref {
+                // CLS
+                print!("\x1B[2J\x1B[1;1H");
+
+                // Victory
+                println!("It was {}!", word_ref);
+                println!("You got it in {} tries.", history.len());
+                return;
+            }
+        }
+
+        // Status Update
+        println!("{}", blank);
+        println!("{} Characters in Word", word_ref.len());
+        println!("Guessed {}", history);
+
+        // Refresh buffer
+        buffer = String::from("");
+
+        println!("What is Character would you like to guess?");
+        while buffer.len() != 1 {
+            io::stdin().read_line(&mut buffer).expect("failed to receive input");
+            buffer = String::from(buffer.trim());
+
+            if buffer.len() == 1 {
+                history += &buffer;
+            } else {
+                buffer = String::from("");
+                println!("Please input only 1 Char");
+            }
+        }
+    }
+}
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Setup dotenv
+    dotenv().expect("dotenv initialization failed");
+
+    // Setup blanks and Get initial word
+    let word = get_random_word().await?;
+
+    game_loop(&word);
+
+    Ok(())
 }
